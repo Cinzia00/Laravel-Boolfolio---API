d214b98ac29c49ee29c8c0c9336b6d80520019a5
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-05-02 15:52:16 +0200

    Fatta api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Route::get('/projects', [ProjectController::class,'index']);
+
+
+Route::get('/test', function () {
+    return response()->json([
+        'name' => 'Cinzia',
+        'role' => 'admin'
+    ]);
 });
